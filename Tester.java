51c372c2476c90f379a4e3f7c82c1273aeffa0c5
bacaa18 <32909697+bacaa18@users.noreply.github.com>
2017-11-06 11:57:53 -0500

    Add files via upload
    havent been able to do much with tester because the fucking Deck() method isnt working ugh also wont let me push normally

---
+++ Tester.java
@@ -0,0 +1,23 @@
+/**
+ * File: Tester.java
+ * @author Andrew Baca, Shep Sims, Peter Blair, Trip Calihan
+ * 
+ * The Tester class for the classes and piles needed to play
+ * a game of Free Cell. 
+ * 
+ * The tester instantiate the piles, deal the cards to them, 
+ * and test all of the methods that implement their behavior. 
+ */
+
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		FreeCellModel game;
+		game = new FreeCellModel();
+		game.reset();
+		game.toString();
+		
+	}
+	
+}
